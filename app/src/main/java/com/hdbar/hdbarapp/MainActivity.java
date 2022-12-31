f3390bc3c6f405f4236af0fb7663789248d77072
shefrum <haykaakaramyan@gmail.com>
2022-12-31 18:44:24 +0400

    switch Activities

--- app/src/main/java/com/hdbar/hdbarapp/MainActivity.java
+++ app/src/main/java/com/hdbar/hdbarapp/MainActivity.java
@@ -3,7 +3,10 @@ package com.hdbar.hdbarapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.hdbar.hdbarapp.databinding.ActivityMainBinding;
@@ -15,12 +18,33 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(R.layout.activity_register);
-        //setContentView(binding.getRoot());
+        setContentView(binding.getRoot());
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        TextView registertextview = (TextView) findViewById(R.id.register_main);
+
+        registertextview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switchActivityes();
+            }
+        });
+
+
     }
+
+
+
+    private void switchActivityes(){/*
+        Intent switchActivityIntent = new Intent(this, Register.class);
+        startActivity(switchActivityIntent);*/
+    }
+
 }
--- app/src/main/java/com/hdbar/hdbarapp/Register.java
+++ app/src/main/java/com/hdbar/hdbarapp/Register.java
@@ -2,10 +2,15 @@ package com.hdbar.hdbarapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Register extends AppCompatActivity {
 
+    private EditText register_username = (EditText) findViewById(R.id.register_usename);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
