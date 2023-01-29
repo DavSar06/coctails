52899880a95e92a511f90bd434e5d1a42db38a39
DavSar06 <mr.davitsargsyan06@gmail.com>
2023-01-29 10:47:11 +0400

    merge

--- app/src/main/java/com/hdbar/hdbarapp/activities/SettingsFragment.java
+++ app/src/main/java/com/hdbar/hdbarapp/activities/SettingsFragment.java
@@ -30,15 +30,6 @@ public class SettingsFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SettingsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static SettingsFragment newInstance(String param1, String param2) {
         SettingsFragment fragment = new SettingsFragment();
         Bundle args = new Bundle();
