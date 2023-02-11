6b2e681bb442392d7aaf608b77670cc5dccf9a88
DavSar06 <mr.davitsargsyan06@gmail.com>
2023-02-11 16:08:07 +0400

    Image choose activity xml changed

--- app/src/main/java/com/hdbar/hdbarapp/activities/ChooseImageActivity.java
+++ app/src/main/java/com/hdbar/hdbarapp/activities/ChooseImageActivity.java
@@ -3,20 +3,12 @@ package com.hdbar.hdbarapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-
-import com.hdbar.hdbarapp.adapters.ImagesAdapter;
 import com.hdbar.hdbarapp.databinding.ActivityChooseImageBinding;
-import com.hdbar.hdbarapp.models.ProfileImage;
 
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 
 public class ChooseImageActivity extends AppCompatActivity {
 
     private ActivityChooseImageBinding binding;
-    private HashMap<String, List<ProfileImage>> images;
 
 
     @Override
@@ -25,20 +17,7 @@ public class ChooseImageActivity extends AppCompatActivity {
         binding = ActivityChooseImageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        images = new HashMap<>();
-        List<ProfileImage> temp = new LinkedList<>();
-        for(int i=0;i<100;i++){
-            if(i!=0 && i%10==0){
-                images.put("Category "+i/10,temp);
-                temp = new LinkedList<>();
-            }else{
-                temp.add(new ProfileImage("image"+i+".jpeg","BlaBla"));
-            }
-        }
 
-        binding.imageListRecyclerView.setAdapter(new ImagesAdapter(images));
-        binding.imageListRecyclerView.setVisibility(View.VISIBLE);
-        binding.progressBar.setVisibility(View.INVISIBLE);
 
         setListeners();
     }
