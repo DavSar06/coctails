20c3301bf57cce5f91b5d9bc760315d326ec31e2
DavSar06 <mr.davitsargsyan06@gmail.com>
2023-02-25 17:43:01 +0400

    Fixed two bags

--- app/src/main/java/com/hdbar/hdbarapp/activities/MainActivity.java
+++ app/src/main/java/com/hdbar/hdbarapp/activities/MainActivity.java
@@ -52,7 +52,6 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationBar();
 
         AlwaysOnRun.AlwaysRun(MainActivity.this);
-        FirebaseAuth.getInstance().signOut();
     }
 
     private void createNotify(){
