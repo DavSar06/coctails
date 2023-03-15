7beeba78109f30ed033e0cec94791c9609973e3c
DavSar06 <mr.davitsargsyan06@gmail.com>
2023-03-15 14:07:31 +0400

    Fixed bag with spaces between cardViews

--- app/src/main/java/com/hdbar/hdbarapp/adapters/CocktailsAdapter.java
+++ app/src/main/java/com/hdbar/hdbarapp/adapters/CocktailsAdapter.java
@@ -1,8 +1,11 @@
 package com.hdbar.hdbarapp.adapters;
 
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
